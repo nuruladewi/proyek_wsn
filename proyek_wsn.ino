1e047537277ad6b1f164e9d5be6368a00019d9db
nuruladewi <nurul.aulia.dewi.tik21@mhsw.pnj.ac.id>
2023-05-19 15:47:50 +0700

    first commit

---
+++ proyek_wsn.ino
@@ -0,0 +1,46 @@
+
+//INI HC-SR04
+#define triggerPin  D6
+#define echoPin     D7
+void setup() {
+  Serial.begin (9600);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
+}
+void loop() {
+  long duration, jarak;
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(triggerPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  jarak = (duration/2) / 29.1;
+  Serial.println("jarak :");
+  Serial.print(jarak);
+  Serial.println(" cm");
+  delay(1000);
+}
+
+
+/*#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+
+void setup() {
+  lcd.begin();
+  lcd.clear();         
+  lcd.backlight();      // Make sure backlight is on
+  
+  // Print a message on both lines of the LCD.
+  lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
+  lcd.print("Hello world!");
+  
+  lcd.setCursor(2,1);   //Move cursor to character 2 on line 1
+  lcd.print("LCD Tutorial");
+}
+
+void loop() {
+}
+*/
